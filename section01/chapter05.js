5a7e4f0c25e39cd1698ed909b9f838572794b88c
leejonghyun93 <fhohffodf93@gmail.com>
2024-09-05 19:11:21 +0900

    1. 변수

---
+++ section01/chapter05.js
@@ -0,0 +1,39 @@
+// 1. Number Type
+let num1 = 27;
+let num2 = 1.5;
+let num3 = -20;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
+
+let inf = Infinity;
+let mInf = -Infinity;
+
+let nan = NaN;
+console.log(1 * "hello");
+
+
+// 2. String Type
+let myName = "이종현"
+let myLocation = "목동";
+let introduce = myName + myLocation;
+
+let introduceText = `${myName}은 ${myLocation}에 거주합니다`;
+console.log(introduceText);
+
+// 템플릿 리터럴 문법
+
+// 3. boolean Type
+let isSwitchOn = true;
+let isEmpty = false;
+
+// 4. Null Type (아무것도 없다)
+let empty = null;
+
+
+// 5. Undefined Type
+let none;
+console.log(none);
