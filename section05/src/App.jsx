e2a80bcce407c74710e630f08b084de95cda4e1a
leejonghyun93 <fhohffodf93@gmail.com>
2024-09-12 19:14:02 +0900

    JSX로 UI표현하기 , State로 사용자 입력 관리하기

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,18 +1,36 @@
 import './App.css'
-import Header from './components/Header.jsx';
-import Main from "./components/Main.jsx";
-import Footer from './components/Footer.jsx';
+// import Header from './components/Header.jsx';
+// import Main from "./components/Main.jsx";
+// import Footer from './components/Footer.jsx';
+// import Button from "./components/Button.jsx";
+// import { useState } from 'react';
 
+// function App() {
+//   const buttonProps = {
+//     text: "메일",
+//     color: "red",
+//     a: 1,
+//     b: 2,
+//     c: 3,
+//   }
+
+//   return (
+//     <>
+//       <Button {...buttonProps}/>
+//       <Button text={"카페"} />
+//       <Button text={"블로그"}><div>자식요소</div></Button> 
+//     </>
+//   );
+// }
+
+import Register from './components/Register';
 
 
 function App() {
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
+      <Register />
     </>
-  )
+  );
 }
-
-export default App
+export default App;
